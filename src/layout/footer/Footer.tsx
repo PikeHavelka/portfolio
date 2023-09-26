c2b28e1784cd7e19185299a772bb9191b0e00a66
Pike Havelka <havelka.pvl@seznam.cz>
2023-09-26 19:32:52 +0200

    footer created

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,33 +1,40 @@
 import "./footer.css"
-
+import { FaGithub, FaFacebookSquare, FaMobileAlt } from "react-icons/fa"
+import { MdEmail } from "react-icons/md"
 
 function Footer() {
   return (
     <footer>
-      <div className="title">
-        <h1>Pavel Havelka</h1>
-        <h2>Portfólio</h2>
+      <div className="container footer-wrapper">
+        <div className="title">
+          <h1>Pavel Havelka</h1>
+          <h2>Portfólio</h2>
+        </div>
+
+        <div className="social-icons">
+          <a href="https://github.com/PikeHavelka">
+            <FaGithub className="github icon" />
+          </a>
+
+          <a href="https://www.facebook.com/PikeHavelka">
+            <FaFacebookSquare className="icon" />
+          </a>
+        </div>
+
+        <div className="contact">
+          <span className="contact-email">
+            <MdEmail className="icon" /> Email:
+            <a href="mailto:havelka.pvl@seznam.cz"> havelka.pvl@seznam.cz</a>
+          </span>
+
+          <span className="contact-mobile">
+            <FaMobileAlt className="icon" /> Mobil:
+            <a href="tel:732162568"> 732 162 568</a>
+          </span>    
+        </div>
+
+        <p className="text-icons" >Na této stránce byly použity ikonky ze stránky</p><a className="link-icons" target="_blank" href="https://icons8.com"> Icons8</a>
       </div>
-
-      <div className="icons">
-        <a href="https://github.com/PikeHavelka">
-
-        </a>
-
-        <a href="https://www.facebook.com/PikeHavelka">
-
-        </a>
-      </div>
-
-      <span>Email:
-        <a href="mailto:havelka.pvl@seznam.cz">havelka.pvl@seznam.cz</a>
-      </span>
-
-      <span>Mobil:
-        <a href="tel:732162568">732 162 568</a>
-      </span>
-      
-      <p>Na stránce byly použity ikony od</p><a target="_blank" href="https://icons8.com">Icons8</a>
     </footer>
   )
 }
