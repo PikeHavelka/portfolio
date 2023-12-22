e4656ab545d1d792d938c819cf0a0c82bbf8aca6
Pike Havelka <havelka.pvl@seznam.cz>
2023-12-22 20:40:30 +0100

    new info about me

--- src/layout/aboutMe/AboutMe.tsx
+++ src/layout/aboutMe/AboutMe.tsx
@@ -6,19 +6,19 @@ function AboutMe() {
       <h1>O mně</h1>
 
     <div className="text">
-      <p>Necelé dva roky se intenzivně zajímám o jazyky určené k frontend vývoji. Jako první značkovací jazyk, který jsem se učil bylo HTML, následně CSS a postupem času se k nim přidal programovací jazyk Javascript. Abych více rozvinul své zkušenosti začal jsem se zajímat o framework React a preprocesor SCSS. Poté přišel na řadu i Typescript.</p>
+      <p>Necelé dva roky se zajímám o frontend vývoj. Stalo se to mým koníčkem, kterému věnuji svůj volný čas. Zajímám se také o jazyky jako HTML, CSS, SCSS, Javscript, Typescript a React. Ovládám velmi základní úpravy fotografií v Photoshopu a k programování využívám Visual Studio Code.</p>
 
       <br />
 
-      <p>Ve svých projektech také využívám některé knihovny jako je Formik, React Router. Nyní testuji i MaterialUI. Samozřejmě se vždy snažím stránky plně aktualizovat, aby fungovaly na všechna zařízení i verze prohlížečů. Responzivita je samozřejmostí.</p>
+      <p>Ve svých projektech také využívám některé knihovny jako jsou Formik nebo React Router. Samozřejmě se vždy snažím stránky plně aktualizovat, aby fungovaly na všechna zařízení i verze prohlížečů. Responzivita je samozřejmostí a optimalizace pro SEO mi není úplně cizí.</p>
 
       <br />
 
-      <p>Zatím své zkušenosti využívám pouze pro vlastní účely, popřípadě k tvorbě webů pro osoby v okolí. Mám za sebou velmi jednoduché weby, díky kterým jsem se dopracoval k těm složitějším. Mé projekty můžete nalézt <a href="https://github.com/PikeHavelka">zde</a>.</p>
+      <p>Zatím své zkušenosti využívám pouze pro vlastní účely, popřípadě k tvorbě webů pro osoby v okolí. Mám za sebou velmi jednoduché weby, díky kterým jsem se propracoval k těm složitějším. Mé projekty můžete nalézt <a href="https://github.com/PikeHavelka">zde</a>.</p>
 
       <br />
       
-      <p>V rozvoji svých zkušeností se plánuji nadále zdokonalovat. A rád bych měl aspoň základní znalosti s backend vývojem, nejlépe node.js.</p>
+      <p>V rozvoji svých zkušeností se plánuji nadále zdokonalovat. Rád bych šel ve studii více do hloubky a naučil se s dalšími jazyky i programy.</p>
     </div>
     </section>
   )
